b1a59bf194c3fc3103e379b4d4746921b94af4f8
Ali895470 <adone0221@gmail.com>
2025-07-20 01:07:08 +0300

    Update page.js

--- app/  edit-post/page.js
+++ app/  edit-post/page.js
@@ -1,13 +1,14 @@
 'use client';
-import ProtectedRoute from '@/lib/ProtectedRoute';
+
+import ProtectedRoute from '../../lib/ProtectedRoute';
 
 export default function EditPostPage() {
   return (
-      <ProtectedRoute>
-            <div>
-                    <h1>تعديل المنشور</h1>
-                            {/* يمكنك وضع نموذج تعديل المنشور هنا */}
-                                  </div>
-                                      </ProtectedRoute>
-                                        );
-                                        }
+    <ProtectedRoute>
+      <div>
+        <h1>تعديل المنشور</h1>
+        {/* تعديل المنشور هنا */}
+      </div>
+    </ProtectedRoute>
+  );
+}
