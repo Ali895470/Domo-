27d02c4a8317e8b31a7fbeb2d78d73462e16e83d
Ali895470 <adone0221@gmail.com>
2025-07-20 02:23:20 +0300

    Update page.js

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,13 +1,14 @@
 'use client';
-import ProtectedRoute from '@/lib/ProtectedRoute';
 
-export default function DashboardPage() {
+import ProtectedRoute from '../../lib/ProtectedRoute';
+
+export default function Dashboard() {
   return (
-      <ProtectedRoute>
-            <div>
-                    <h1>لوحة التحكم</h1>
-                            {/* يمكنك عرض إحصائيات أو بيانات هنا */}
-                                  </div>
-                                      </ProtectedRoute>
-                                        );
-                                        }
+    <ProtectedRoute>
+      <div>
+        <h1>Dashboard</h1>
+        {/* ضع هنا أي مكونات أو محتوى خاص بالداشبورد */}
+      </div>
+    </ProtectedRoute>
+  );
+}
