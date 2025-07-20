1f412f68b10e87c221a5fd9457dbf377cfd74490
Ali895470 <adone0221@gmail.com>
2025-07-20 16:40:14 +0300

    Update layout.js

--- app/  edit-post/layout.js
+++ app/  edit-post/layout.js
@@ -3,20 +3,16 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../../firebaseconfig'; // المسار الصحيح ✅
-
+ import { auth } from 
+'../../firebaseconfig';//  المسار الصحيح
 export default function Layout({ children }) {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-      
-      if (!currentUser) {
-        router.push('/');
+ const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{  setUser(currentUser);  setLoading(false);  
+ if (!currentUser){ router.push('/'):
       }
     });
 
