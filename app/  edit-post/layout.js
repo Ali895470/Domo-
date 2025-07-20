4779adc265720c2f84f7ca4c13b8426bb818f528
Ali895470 <adone0221@gmail.com>
2025-07-20 03:57:39 +0300

    Update layout.js

--- app/  edit-post/layout.js
+++ app/  edit-post/layout.js
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../../firebaseconfig';
+import { auth } from '@/lib/firebaseConfig'; // استخدم هذا المسار
 
 export default function Layout({ children }) {
   const router = useRouter();
