d37f5d3675f0ec1e3d1d5ac24031d5efa23a13b9
Ali895470 <adone0221@gmail.com>
2025-07-22 16:08:43 +0300

    Update page.js

--- app/Login/page.js
+++ app/Login/page.js
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/firebaseConfig';
+import { auth } from '@/firebaseconfig';
 
 export default function LoginPage() {
   const router = useRouter();
