b0fb023d568908acdc0ac95cafeeaa40346bfc09
Ali895470 <adone0221@gmail.com>
2025-07-23 21:45:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').Nextconfig} */
+const nextconfig = {};
 
-module.exports = nextConfig;
+module.exports = nextconfig;
